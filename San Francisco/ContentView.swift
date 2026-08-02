c64b89af0377cbe8b1c2d78240405611c9c10667
jurre111 <jurredijkstra@mijnheem.nl>
2026-08-02 23:42:41 +0200

    test

--- San Francisco/ContentView.swift
+++ San Francisco/ContentView.swift
@@ -1,17 +1,68 @@
 import SwiftUI
 
-struct ContentView: View {
-    @State private var symbols: [String: [String]] = []
-    var body: some View {
-        List {
 
+struct Symbol: Codable {
+    var categories: [String]
+    var availability: String
+}
+
+struct Category: Codable {
+    var displayName: String
+    var displayIcon: String
+    var symbols: [String]
+}
+
+struct ContentView: View {
+    @State private var symbols: [String: Symbol] = [:]
+    @State private var categories: [String: Category] = [:]
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(categories.keys.sorted(), id: \.self) { category in
+                    if let catInfo = categories[category] {
+                        NavigationLink(catInfo.displayName) {
+                            List {
+                                ForEach(catInfo.symbols, id: \.self) { symbol in
+                                    if let symbolInfo = symbols[symbol] {
+                                        HStack(spacing: 10) {
+                                            Image(systemName: symbol)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 30, height: 30)
+                                            Text(symbol)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+        .navigationTitle("San Francisco")
         .onAppear {
-            let url = Bundle.main.url(forResource: "symbols", withExtension: "plist")
+            guard let url = Bundle.main.url(forResource: "symbols", withExtension: "plist") else {
+                return
+            }
             let data = try Data(contentsOf: url)
-            let dict = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+            symbols = try PropertyListDecoder().decode([String: Symbol].self, from: data)
+
+            categories = loadCategories(symbols)
         }
     }
+}
+
+func loadCategories(_ symbols: [String: Symbol]) -> [String: Category] {
+    var categories: [String: Category] = [:]
+    for (symbol, info) in symbols {
+        for category in info.categories {
+            if categories[category] == nil {
+                categories[category] = Category(displayName: category, displayIcon: "", symbols: [])
+            }
+            categories[category]!.symbols.append(symbol)
+        }
+    }
+    return categories
 }
 
 #Preview {
