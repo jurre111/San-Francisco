aa240058cba5e54f95c45df71256ec8ea2366887
jurre111 <jurredijkstra@mijnheem.nl>
2026-08-10 18:47:20 +0200

    test

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import plistlib
 from operator import itemgetter
-import os
-from fontTools.ttLib import TTFont
+import json
+import subprocess
 
 path = "/System/Library/PrivateFrameworks/SFSymbols.framework/Resources/CoreGlyphs.bundle/Contents/Resources"
 
-font = TTFont(f"/home/jurre/Downloads/SFSymbols/SF Symbols/SFSymbols.pkg/Payload (2)/Library/Fonts/SF-Pro.ttf")
-glyphs = font.getGlyphOrder()
-print(f"{len(glyphs)} glyphs")
+result = subprocess.run(["sfsym", "list", "--json"], capture_output=True, text=True)
+symbolsInfo = json.loads(result.stdout)
+for index, symbol in enumerate(symbolsInfo):
+    result = subprocess.run(["sfsym", "info", symbol], capture_output=True, text=True)
+    info = json.loads(result.stdout)
+    symbolsInfo[index] = info
+
+print(symbolsInfo)
 exit(1)
+
 
 with open(f"{path}/symbol_categories.plist", "rb") as f:
     symbol_categories = plistlib.load(f)
