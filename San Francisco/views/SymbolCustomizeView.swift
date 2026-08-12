82ca58cc78d3f671cc0fd99e2ba5c28bd10abf42
jurre111 <jurredijkstra@mijnheem.nl>
2026-08-12 17:38:40 +0200

    test

--- San Francisco/views/SymbolCustomizeView.swift
+++ San Francisco/views/SymbolCustomizeView.swift
@@ -8,111 +8,19 @@
 import SwiftUI
 
 struct SymbolCustomizeView: View {
-    @ObservedObject var mgr: sfmgr = sfmgr.shared
-    @State private var infoSheet: sfmgr.Symbol? = nil
-    @State private var renderingMode: String = "default"
-    @State private var color: Color = .blue
     @State private var isFavorite: Bool = false
-    @State private var gradientOn = false
-    @State private var variableColorOn = false
     let symbol: sfmgr.Symbol
 
     var body: some View {
-        NavigationStack {
-            List{
-                ZStack {
-                    Color.clear
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .aspectRatio(1.0, contentMode: .fit)
-                    Image(systemName: symbol.name)
-                        .symbolRenderingMode(getRenderingMode(from: renderingMode))
-                        .font(.system(size: 220))
-                        .foregroundColor(color)
-                }
-                .listRowBackground(
-                    RoundedRectangle(cornerRadius: 28, style: .continuous)
-                        .fill(Color(UIColor.secondarySystemGroupedBackground))
-                )
-                Section {
-                    Picker(selection: $renderingMode) {
-                        Text("Default").tag("default")
-                        if symbol.categories.contains("multicolor") {
-                            Text("Multicolor").tag("multicolor")
-                        }
-                        Text("Hierarchical").tag("hierarchical")
-                        Text("Palette").tag("palette")
-                        Text("Monochrome").tag("monochrome")
-                    } label: {
-                        HStack(spacing: 12) {
-                            Image(systemName: "paintpalette")
-                            Text("Rendering Mode")
-                        }
-                    }
-
-                    if sfmgr.shared.systemVersion >= 26.0 {
-                        Toggle(isOn: $gradientOn) {
-                            HStack(spacing: 12) {
-                                Image(systemName: "circle.fill")
-                                    .foregroundStyle(.blue)
-                                    .symbolColorRenderingMode(.gradient)
-                            }
-                        }
-                    }
-                }
-                Section {
-                    HStack(spacing: 12) {
-                        Text("Color")
-                        Spacer()
-                        ColorPicker("Color", selection: $color)
-                            .labelsHidden()
-                            .frame(width: 40)
-                    }
-                }
-            }
-            .navigationTitle("Customize")
-            .navigationBarTitleDisplayMode(.inline)
-            .sheet(item: $infoSheet) { symbol in
-                SymbolInfoView(symbol: symbol)
-            }
-            .toolbar {
-                Button {
-                    sfmgr.shared.toggleFavorite(symbol: symbol.name)
-                    isFavorite.toggle()
-                } label: {
-                    Image(systemName: isFavorite ? "star.fill" : "star")
-                }
-                Button {
-                    infoSheet = symbol
-                } label: {
-                    Image(systemName: "info.circle")
-                }
-            }
-            .onAppear {
-                isFavorite = sfmgr.shared.favorites.contains(symbol.name)
-            }
-        }
-    }
-
-    func getRenderingMode(from string: String) -> SymbolRenderingMode? {
-        switch string {
-            case "default":
-                return nil
-            case "multicolor":
-                return .multicolor
-            case "hierarchical":
-                return .hierarchical
-            case "palette":
-                return .palette
-            case "monochrome":
-                return .monochrome
-            default:
-                return nil
+        if #available(iOS 26.0, *) {
+            iOS26(symbol: symbol)
+        } else if #available(iOS 18.0, *) {
+            iOS18(symbol: symbol)
         }
     }
 }
 
 struct iOS26: View {
-    @ObservedObject var mgr: sfmgr = sfmgr.shared
     @State private var infoSheet: sfmgr.Symbol? = nil
     @State private var renderingMode: String = "default"
     @State private var color: Color = .blue
@@ -266,7 +174,6 @@ struct iOS26: View {
 }
 
 struct iOS18: View {
-    @ObservedObject var mgr: sfmgr = sfmgr.shared
     @State private var infoSheet: sfmgr.Symbol? = nil
     @State private var renderingMode: String = "default"
     @State private var color: Color = .blue
@@ -408,3 +315,20 @@ struct iOS18: View {
         }
     }
 }
+
+func getRenderingMode(from string: String) -> SymbolRenderingMode? {
+        switch string {
+            case "default":
+                return nil
+            case "multicolor":
+                return .multicolor
+            case "hierarchical":
+                return .hierarchical
+            case "palette":
+                return .palette
+            case "monochrome":
+                return .monochrome
+            default:
+                return nil
+        }
+    }
